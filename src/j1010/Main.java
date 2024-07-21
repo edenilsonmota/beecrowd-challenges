c98d7eea8b835efa5a6c7e8b68869b9812699580
Edenilson Mota <edenilsonmota.job@gmail.com>
2024-07-21 14:18:17 -0300

    add challenge 1010

---
+++ src/j1010/Main.java
@@ -0,0 +1,25 @@
+package j1010;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        int idProduto = sc.nextInt();
+        int quantProduto = sc.nextInt();
+        double precoProduto = sc.nextDouble();
+
+        int idProdutoDois = sc.nextInt();
+        int quantProdutoDois = sc.nextInt();
+        double precoProdutoDois = sc.nextDouble();
+
+        double total = (quantProduto * precoProduto) + (quantProdutoDois * precoProdutoDois);
+
+
+        System.out.println("VALOR A PAGAR: R$ " + String.format("%.2f", total));
+
+        sc.close();
+        
+    }
+}
